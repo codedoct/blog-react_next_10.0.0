825b1bd8e54fac3e81f55fff0ad3da4c25ec5f8b
victra <victra.sonata@gmail.com>
2020-11-01 09:49:46 +0700

    fix css serverside

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import Head from 'next/head'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { StylesProvider } from '@material-ui/styles'
@@ -7,11 +8,20 @@ import '../styles/globals.css'
 import PropTypes from 'prop-types'
 
 const MyApp = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentElement.removeChild(jssStyles)
+    }
+  }, [])
+
   return (
     <>
       <Head>
         <title>Put your title here</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
         <StylesProvider injectFirst>
