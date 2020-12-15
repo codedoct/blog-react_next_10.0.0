318a62f27cc579d19cc99fbab1fae95a95a7d2c9
victra <victra.sonata@gmail.com>
2020-12-16 05:10:49 +0700

    fix next config read env file

--- next.config.js
+++ next.config.js
@@ -1,14 +1,17 @@
+const webpack = require('webpack')
 const path = require('path')
 require('dotenv').config()
 
 module.exports = {
-  env: {
-    BASE_URL: process.env.BASE_URL,
-    API_VERSION: process.env.API_VERSION
-  },
-
   webpack: config => {
+    const env = Object.keys(process.env).reduce((acc, curr) => {
+      acc[`process.env.${curr}`] = JSON.stringify(process.env[curr])
+      return acc
+    }, {})
+    config.plugins.push(new webpack.DefinePlugin(env))
+
     config.resolve.alias['~'] = path.join(__dirname, './')
+
     return config
   }
 }
